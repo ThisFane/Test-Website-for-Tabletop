b936a4992bf6f23ac3d4d09fdc1e85b094fdcb5c
Merl <thisfane@gmx.de>
2026-02-15 03:48:08 +0100

    now it works

--- js/loginChecker.js
+++ js/loginChecker.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
     const sessionData = getSession();
     if (sessionData) {
         console.log('Welcome back, ' + sessionData.username);
-        document.documentElement.style.display = "block";
+        document.body.style.display = "block";
     } else {
         console.log('No active session.');
         window.location.href = "index.html";
